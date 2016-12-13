a016923ebbf80a44e11687d19043fdbaf8567c22
Naz <admin@admins-MacBook-Pro.local>
2016-12-13 20:03:59 +0000

    Added Lesson2 - Exercise 1

---
+++ src/main/io/github/nazcompile/Lesson2.java
@@ -0,0 +1,29 @@
+package io.github.nazcompile;
+
+import java.util.List;
+
+public class Lesson2 {
+	
+	/**
+	 * All exercises should be completed using Lambda expressions and the new
+	 * methods added to JDK 8 where appropriate. There is no need to use an
+	 * explicit loop in any of the code. Use method references rather than full
+	 * lambda expressions wherever possible.
+	 */
+	
+	  /**
+	   * Exercise 1
+	   *
+	   * Create a new list with all the strings from original list converted to 
+	   * lower case and print them out.
+	 * @return 
+	   */
+	  public List<String> exercise1(List<String> list) {
+		  List<String> result = null;
+
+	    /* YOUR CODE HERE */
+		 return result;
+	  }
+
+
+}
---
+++ src/test/io/github/nazcompile/Lesson2Spec.groovy
@@ -0,0 +1,17 @@
+package io.github.nazcompile
+
+import spock.lang.Specification
+
+class Lesson2Spec extends Specification {
+	
+	Lesson2 lesson2
+	
+	def setup() {
+		lesson2 = new Lesson1()
+	}
+
+	def "Should return new list with all the strings from original list converted to lower case"() {
+			
+	}
+
+}
